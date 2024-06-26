c674988a58edcbafd394d46f995f65d49e4bfeb1
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-26 16:14:57 +0530

    add hero component

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+const Hero = () => {
+  return (
+    <div className="hero_section">
+      <div className="hero_section_text">
+        <h1>
+          <span className="highlight">Hi,</span>
+          <span className="name">I'm Harshit Verma</span>
+        </h1>
+        <h2>A Front-end Developer and Quality Analyst</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
