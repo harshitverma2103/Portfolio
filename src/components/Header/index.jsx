ead370f28937cb3a506c97843fd50c9fe7429841
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-26 16:01:42 +0530

    add toggle icon for mobile view

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,64 @@
 import React, { useState } from "react";
 import logo from "../../assets/portfolio_logo.png";
+import menu_icon from "../../assets/icons/menu_icon.png";
 import "./style.css";
 
 const Header = () => {
   const [underline, setUnderline] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const toggleUnderline = () => {
     setUnderline(!underline);
+  };
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
   };
 
   return (
     <header className="header">
       <div className="header_item">
         <img className="header_logo_img" src={logo} alt="Logo" />
-        <ul className="header_links">
-          <li className={`header_link ${underline ? "underline" : ""}`} onMouseEnter={toggleUnderline} onMouseLeave={toggleUnderline}>Home</li>
-          <li className={`header_link ${underline ? "underline" : ""}`} onMouseEnter={toggleUnderline} onMouseLeave={toggleUnderline}>About</li>
-          <li className={`header_link ${underline ? "underline" : ""}`} onMouseEnter={toggleUnderline} onMouseLeave={toggleUnderline}>Project</li>
-          <li className={`header_link ${underline ? "underline" : ""}`} onMouseEnter={toggleUnderline} onMouseLeave={toggleUnderline}>Skills</li>
+        <button className="mobile_toggle_button" onClick={toggleMenu}>
+          <img src={menu_icon} alt="Menu" />
+        </button>
+        <ul className={`header_links ${showMenu ? "show" : ""}`}>
+          <li
+            className="header_link"
+            onMouseEnter={toggleUnderline}
+            onMouseLeave={toggleUnderline}
+          >
+            <a href="/">Home</a>
+          </li>
+          <li
+            className="header_link"
+            onMouseEnter={toggleUnderline}
+            onMouseLeave={toggleUnderline}
+          >
+            <a href="/">About</a>
+          </li>
+          <li
+            className="header_link"
+            onMouseEnter={toggleUnderline}
+            onMouseLeave={toggleUnderline}
+          >
+            <a href="/">Project</a>
+          </li>
+          <li
+            className="header_link"
+            onMouseEnter={toggleUnderline}
+            onMouseLeave={toggleUnderline}
+          >
+            <a href="/">Skills</a>
+          </li>
+          <li
+            className="header_link"
+            onMouseEnter={toggleUnderline}
+            onMouseLeave={toggleUnderline}
+          >
+            <a href="/">Contact me</a>
+          </li>
         </ul>
-        <button className="contact_button">Contact me</button>
       </div>
     </header>
   );
