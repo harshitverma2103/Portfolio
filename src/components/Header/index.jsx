4a305702e5f66a0cf3fce79e2936039f2a7597d7
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-26 14:27:55 +0530

    Add header styling with hover underline effect and contact button styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/portfolio_logo.png";
-import "./style.css"
+import "./style.css";
 
 const Header = () => {
+  const [underline, setUnderline] = useState(false);
+
+  const toggleUnderline = () => {
+    setUnderline(!underline);
+  };
+
   return (
     <header className="header">
       <div className="header_item">
         <img className="header_logo_img" src={logo} alt="Logo" />
-        <ul className="header_links">  
-          <li className="header_link">Home</li>
-          <li className="header_link">About</li>
-          <li className="header_link">Project</li>
-          <li className="header_link">Skills</li>
+        <ul className="header_links">
+          <li className={`header_link ${underline ? "underline" : ""}`} onMouseEnter={toggleUnderline} onMouseLeave={toggleUnderline}>Home</li>
+          <li className={`header_link ${underline ? "underline" : ""}`} onMouseEnter={toggleUnderline} onMouseLeave={toggleUnderline}>About</li>
+          <li className={`header_link ${underline ? "underline" : ""}`} onMouseEnter={toggleUnderline} onMouseLeave={toggleUnderline}>Project</li>
+          <li className={`header_link ${underline ? "underline" : ""}`} onMouseEnter={toggleUnderline} onMouseLeave={toggleUnderline}>Skills</li>
         </ul>
         <button className="contact_button">Contact me</button>
       </div>
